da64c91671d7474b80f12174fe5f3e8ed6fd9b15
YukaYoshizawa <s1324243@cst.otsuma.ac.jp>
2026-06-29 23:53:32 +0900

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const io = new Server(server);
 
 const path = require('path');
 
-app.use((express.staticpath.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 app.get('/liveplanner.html', (req, res) => {
